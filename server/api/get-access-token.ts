72993309fdb8377a75539b5b055a38b2784c91f0
gammelinne <kylian.renault@viacesi.fr>
2025-03-19 14:06:03 +0100

    test token

--- server/api/get-access-token.ts
+++ server/api/get-access-token.ts
@@ -2,13 +2,18 @@ export default defineEventHandler(async (event) => {
     const config = useRuntimeConfig();
     const clientId = config.spotifyClientId;
     const clientSecret = config.spotifyClientSecret;
+    if(!clientId || !clientSecret) {
+        return { error: "Missing Spotify client ID or client secret" };
+    }
 
+    console.log(clientId, clientSecret)
     const body = new URLSearchParams();
     body.append("grant_type", "client_credentials");
     body.append("client_id", clientId);
     body.append("client_secret", clientSecret);
 
     try {
+        console.log("Fetching access token from Spotify");
         const response = await fetch("https://accounts.spotify.com/api/token", {
             method: "POST",
             headers: {
